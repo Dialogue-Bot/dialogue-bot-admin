e2a10e28253c543ace2ea1ef24668635dc1e7de5
Vo Hoang Huy <hoanghuy.dev0210@gmail.com>
2024-04-12 01:22:37 +0700

    feat: update translations

--- client/src/components/layouts/publish/header.tsx
+++ client/src/components/layouts/publish/header.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-  const { t } = useTranslation('landingPage')
+  const { t } = useTranslation('layout')
 
   return (
     <header className='h-header flex items-center fixed top-0 left-0 right-0 bg-background z-50'>
@@ -18,9 +18,19 @@ const Header = () => {
           <LogoText className='text-primary text-xl' />
         </Link>
 
-        <Link to={ROUTES.AUTH.LOGIN} className={buttonVariants()}>
-          {t('start_now')}
-        </Link>
+        <div>
+          <Link
+            to={ROUTES.PUBLIC.HELP}
+            className={buttonVariants({
+              variant: 'link',
+            })}
+          >
+            {t('public_layout.help')}
+          </Link>
+          <Link to={ROUTES.AUTH.LOGIN} className={buttonVariants()}>
+            {t('public_layout.start_now')}
+          </Link>
+        </div>
       </div>
     </header>
   )
--- client/src/pages/help-detail.tsx
+++ client/src/pages/help-detail.tsx
@@ -1,17 +1,17 @@
 import {
+  Cell,
   H1,
-  Link,
   H2,
   H3,
   H4,
-  TypographyP,
-  TypographyBlockquote,
-  Table,
-  Cell,
   Head,
   HeadCell,
-  Row,
+  Link,
   OrderedList,
+  Row,
+  Table,
+  TypographyBlockquote,
+  TypographyP,
   UnOrderList,
 } from '@/components/typography'
 import { buttonVariants } from '@/components/ui'
@@ -21,9 +21,9 @@ import { cn } from '@/lib/utils'
 import { format } from 'date-fns'
 import { ArrowLeft, ArrowRight } from 'lucide-react'
 import {
-  useLoaderData,
   Link as RLink,
   ScrollRestoration,
+  useLoaderData,
 } from 'react-router-dom'
 
 const HelpDetail = () => {
@@ -34,78 +34,82 @@ const HelpDetail = () => {
   }
 
   return (
-    <div className='container py-6'>
-      <ScrollRestoration />
-      <div className='max-w-3xl mx-auto'>
-        <div>
-          <div className='mb-8'>
-            <H1 className='mb-4'>{data.article.title}</H1>
-            <div className='flex items-center gap-2'>
-              <span className='text-muted-foreground text-sm'>
-                {format(new Date(data.article.date), 'MMMM dd, yyyy')}
-              </span>
-              <span className='font-semibold text-sm'>
-                By {data.article.author}
-              </span>
-            </div>
-          </div>
+    <div className='pt-header'>
+      <div className='container py-6'>
+        <ScrollRestoration />
+        <div className='max-w-3xl mx-auto'>
           <div>
-            {data.article.content({
-              components: {
-                a: (props: any) => (
-                  <Link
-                    {...props}
-                    to={(props.href as string) || props?.to || '#'}
-                  />
-                ),
-                h1: (props: any) => <H1 {...props} />,
-                h2: (props: any) => <H2 {...props} />,
-                h3: (props: any) => <H3 {...props} />,
-                h4: (props: any) => <H4 {...props} />,
-                p: (props: any) => <TypographyP {...props} />,
-                blockquote: (props: any) => <TypographyBlockquote {...props} />,
-                ul: (props: any) => <UnOrderList {...props} />,
-                table: (props: any) => <Table {...props} />,
-                th: (props: any) => <HeadCell {...props} />,
-                tr: (props: any) => <Row {...props} />,
-                td: (props: any) => <Cell {...props} />,
-                thead: (props: any) => <Head {...props} />,
-                ol: (props: any) => <OrderedList {...props} />,
-              },
-            })}
-          </div>
-
-          {data.prev || data.next ? (
-            <div>
-              <hr className='my-8' />
-              <div className='flex flex-col gap-4'>
-                {data.prev ? (
-                  <RLink
-                    to={`${ROUTES.PUBLIC.HELP}/${data.prev.slug}`}
-                    className={cn(
-                      buttonVariants({ variant: 'outline' }),
-                      'min-h-16 justify-start gap-4',
-                    )}
-                  >
-                    <ArrowLeft />
-                    <span>{data.prev.title}</span>
-                  </RLink>
-                ) : null}
-                {data.next ? (
-                  <RLink
-                    to={`${ROUTES.PUBLIC.HELP}/${data.next.slug}`}
-                    className={cn(
-                      buttonVariants({ variant: 'outline' }),
-                      'min-h-16 justify-end gap-4',
-                    )}
-                  >
-                    <span>{data.next.title}</span>
-                    <ArrowRight />
-                  </RLink>
-                ) : null}
+            <div className='mb-8'>
+              <H1 className='mb-4'>{data.article.title}</H1>
+              <div className='flex items-center gap-2'>
+                <span className='text-muted-foreground text-sm'>
+                  {format(new Date(data.article.date), 'MMMM dd, yyyy')}
+                </span>
+                <span className='font-semibold text-sm'>
+                  By {data.article.author}
+                </span>
               </div>
             </div>
-          ) : null}
+            <div>
+              {data.article.content({
+                components: {
+                  a: (props: any) => (
+                    <Link
+                      {...props}
+                      to={(props.href as string) || props?.to || '#'}
+                    />
+                  ),
+                  h1: (props: any) => <H1 {...props} />,
+                  h2: (props: any) => <H2 {...props} />,
+                  h3: (props: any) => <H3 {...props} />,
+                  h4: (props: any) => <H4 {...props} />,
+                  p: (props: any) => <TypographyP {...props} />,
+                  blockquote: (props: any) => (
+                    <TypographyBlockquote {...props} />
+                  ),
+                  ul: (props: any) => <UnOrderList {...props} />,
+                  table: (props: any) => <Table {...props} />,
+                  th: (props: any) => <HeadCell {...props} />,
+                  tr: (props: any) => <Row {...props} />,
+                  td: (props: any) => <Cell {...props} />,
+                  thead: (props: any) => <Head {...props} />,
+                  ol: (props: any) => <OrderedList {...props} />,
+                },
+              })}
+            </div>
+
+            {data.prev || data.next ? (
+              <div>
+                <hr className='my-8' />
+                <div className='flex flex-col gap-4'>
+                  {data.prev ? (
+                    <RLink
+                      to={`${ROUTES.PUBLIC.HELP}/${data.prev.slug}`}
+                      className={cn(
+                        buttonVariants({ variant: 'outline' }),
+                        'min-h-16 justify-start gap-4',
+                      )}
+                    >
+                      <ArrowLeft />
+                      <span>{data.prev.title}</span>
+                    </RLink>
+                  ) : null}
+                  {data.next ? (
+                    <RLink
+                      to={`${ROUTES.PUBLIC.HELP}/${data.next.slug}`}
+                      className={cn(
+                        buttonVariants({ variant: 'outline' }),
+                        'min-h-16 justify-end gap-4',
+                      )}
+                    >
+                      <span>{data.next.title}</span>
+                      <ArrowRight />
+                    </RLink>
+                  ) : null}
+                </div>
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
