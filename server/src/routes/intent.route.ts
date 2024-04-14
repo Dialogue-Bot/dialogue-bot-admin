25fb5b2af3ec5acf1f83f15fbee0e4c73f876f68
khanh <khanhdt.game@gmail.com>
2024-04-14 14:51:01 +0700

    update predict intent

--- server/src/routes/intent.route.ts
+++ server/src/routes/intent.route.ts
@@ -25,7 +25,6 @@ export class IntentRoute implements Routes {
     this.router.post(
       ENDPOINTS.INTENT.PREDICT,
       validate(PredictIntentDTO, 'body'),
-      authMiddleware,
       this.controller.predictIntent,
     )
 
