5646a751c8e3dd4c4ff5a0fceab3fae4b3b91a48
khanh <khanhdt.game@gmail.com>
2024-05-13 00:43:16 +0700

    DIAL-44-Implement-connect-agent-for-user

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -104,7 +104,6 @@ export const SOCKET_EVENTS = {
   RECEIVED: 'received',
   DISCONNECT: 'disconnect',
   DISABLED_BOT: 'disabled-bot',
-  NOTIFICATION_CONNECT_AGENT: 'notification-connect-agent',
   AGENT_MESSAGE: 'agent-message',
 }
 
@@ -114,5 +113,4 @@ export const BOT_EVENT = {
   MESSAGE: 'message',
   TYPING: 'typing',
   STOP_TYPING: 'stop-typing',
-  CONNECT_AGENT: 'connect-agent',
 }
