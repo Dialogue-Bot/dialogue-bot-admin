f27d4bb6c99976652bf14ec05466fdf8a67369e7
Vo Hoang Huy <hoanghuy.dev0210@gmail.com>
2024-05-18 22:46:07 +0700

    .env.vault

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,5 +17,17 @@ export default defineConfig(async () => {
         auto: true,
       }),
     ],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: 5173, // you can replace this port with any port
+    },
+    preview: {
+      port: 5173,
+      host: true,
+    },
   }
 })
