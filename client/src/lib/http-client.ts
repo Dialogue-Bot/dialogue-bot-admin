06aabc4a738749075be3b5f3f87ef96fa0f98c42
Vo Hoang Huy <hoanghuy.dev0210@gmail.com>
2024-05-20 23:26:36 +0700

    update

--- client/src/lib/http-client.ts
+++ client/src/lib/http-client.ts
@@ -4,7 +4,11 @@ import { ELang } from '@/types/share'
 import axios from 'axios'
 
 const http_client = axios.create({
-  baseURL: `${import.meta.env.VITE_PROD_API_URL}/api`,
+  baseURL: `${
+    import.meta.env.DEV
+      ? import.meta.env.VITE_DEV_API_URL
+      : import.meta.env.VITE_PROD_API_URL
+  }/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
