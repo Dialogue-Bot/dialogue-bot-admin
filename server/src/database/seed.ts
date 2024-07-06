9230e3b5f8a2c7ef7eb4520e19ee0b46bd2973f8
khanh <khanhdt.game@gmail.com>
2024-07-06 22:37:58 +0700

    update seed data

--- server/src/database/seed.ts
+++ server/src/database/seed.ts
@@ -53,59 +53,11 @@ async function seedFlows() {
   } catch (error) {
     console.error(`Can't create default flows template`)
   }
-
-
 }
-
-// async function seedIntents() {
-//   const intentService = Container.get(IntentService)
-
-//   const templates = loadTemplates()
-
-//   const intents = templates
-//     .map((template: any) => {
-//       return template.intents
-//     })
-//     .flat()
-
-//   try {
-//     await intentService.seedIntents(intents)
-//   } catch (error) {
-//     console.error(`Can't seed intents`)
-//   }
-// }
-
-// async function seedFlows() {
-//   const flowService = Container.get(FlowService)
-//   const templates = loadTemplates()
-
-//   const mainFlows = templates.map((template: any) => {
-//     return template.mainFlow
-//   })
-
-//   const subFlows = templates.map((template: any) => {
-//     return template.subFlows
-//   })
-
-//   const admin = await db.query.users.findFirst({
-//     where: eq(users.email, 'admin@gmail.com'),
-//   })
-
-//   try {
-//     await flowService.seedFlows(admin.id, [
-//       ...mainFlows.flat(),
-//       ...subFlows.flat(),
-//     ])
-//   } catch (error) {
-//     console.error(`Can't seed flows`)
-//   }
-// }
-
 
 async function main() {
   await seedChannelTypes()
   await seedDefaultAccount()
-  // await seedIntents()
   await seedFlows()
 
   console.log('Seed data successfully')
